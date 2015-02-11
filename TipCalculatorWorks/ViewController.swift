711d427815f2aa2e9483284fdaa65283ecfdc94d
Lea Sparkman <leaspark@Leas-MacBook-Pro.local>
2015-02-11 13:32:44 -0800

    Initial Commit

---
+++ TipCalculatorWorks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TipCalculatorWorks
+//
+//  Created by Lea Sparkman on 2/11/15.
+//  Copyright (c) 2015 CompSci. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
